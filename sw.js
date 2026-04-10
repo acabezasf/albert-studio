13ad555cb72e45e3eebffa8bc828c5748b5fe0be
acabezasf <aacabezasff@gmail.com>
2026-04-10 23:22:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'albert-studio-v1';
+const urlsToCache = [
+  './albert_studio_app.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Devuelve del cache si existe, si no, baja de red (ideal offline-first mix)
+      return response || fetch(event.request);
+    }).catch(() => {
+        return caches.match('./albert_studio_app.html');
+    })
+  );
+});
